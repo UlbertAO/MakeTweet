97d701d1eb26592dc6fbe44a83a67e948191dfca
ulbertAO <sp595993@gmail.com>
2023-11-04 08:51:33 +0530

    ng-bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { AppConfigService } from './core/services/app-config.service';
 import { APP_INITIALIZER } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
   providers: [
     {
       provide: APP_INITIALIZER,
